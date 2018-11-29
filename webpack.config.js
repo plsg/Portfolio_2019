d1b1f9e250a5f7d730cab8fd659136dc01fad2c6
Patrick Lesage <Patrick.Lesage@mnp.ca>
2018-11-28 22:06:22 -0500

    favicons

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+let FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
     plugins: [
+        new FaviconsWebpackPlugin('./src/ma-main.jpg'),
         new HtmlWebpackPlugin({
             title: "Webpack demo",
+            template: './src/index.html',
         }),
     ],
 };
