aa16dbf60f53d6b8889d816cc5c9c02802271800
Patrick Lesage <Patrick.Lesage@mnp.ca>
2018-11-29 20:22:31 -0500

    updated html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import component from "./component";
-document.body.appendChild(component());
+// import component from "./component";
+// document.body.appendChild(component());
 
 const currentDate = new Date().toLocaleString();
 console.log('%c' + `build ok: ${currentDate}`, 'background: lime; color: green');
